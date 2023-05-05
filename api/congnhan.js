67b6cd90ee72b56568aeec3b2989e701c2a1ef70
Your Name <you@example.com>
2023-05-05 13:44:28 +0700

    commit

--- api/congnhan.js
+++ api/congnhan.js
@@ -98,6 +98,23 @@ router.get("/baocaothangtheoto", async (req, res) => {
   }
 });
 
+// báo cáo chấm công theo phân xưởng
+router.get("/baocaochamcongthangphanxuong", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool
+      .request()
+      .input("mato", req.query.mato)
+      .input("nam", req.query.nam)
+      .input("thang", req.query.thang)
+      .execute('bangchamcongthang_phanxuong')
+    const cn = result.recordset;
+    res.json(cn);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // all cong nhan
 router.get("/allcongnhan", async (req, res) => {
   try {

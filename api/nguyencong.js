1af97c3c3dcaacdf53ee691c341e515265657f7e
Your Name <you@example.com>
2023-05-23 20:01:39 +0700

    commit

--- api/nguyencong.js
+++ api/nguyencong.js
@@ -279,6 +279,31 @@ router.get("/filterfulldmnc", async (req, res) => {
   }
 });
 
+// mapx & nhomsp
+router.get("/filterfulldmncmapxandnhomsp", async (req, res) => {
+  try {
+    const mapxList = req.query.mapx;
+    // console.log(mapxList);
+    const strpx = "'" + mapxList.join("','") + "'";
+    // console.log(strpx);
+    const masp = req.query.masp;
+    // console.log(masp);
+    const nhomsp = req.query.nhomsp;
+    // console.log(nhomsp);
+
+    await pool.connect();
+    const result = await pool
+      .request()
+      .query(
+        `select * from dmnc where mapx in (${strpx}) and nhomsp='${nhomsp}'`
+      );
+    const data = result.recordset;
+    res.json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // cập nhật nguyên công
 router.patch("/nc/:_id", async (req, res) => {
   try {

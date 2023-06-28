d118e3f2d2b964f4336803ae3cc904387bf327dc
Your Name <you@example.com>
2023-06-28 14:59:52 +0700

    commit

--- api/lokehoach.js
+++ api/lokehoach.js
@@ -257,6 +257,37 @@ router.get("/updatestatuslonhamay", async (req, res) => {
   }
 });
 
+// cập nhật status = 3 và ngayketthucthucte cho lô kế hoạch
+router.patch("/updatestatusngayhoanthanhtt/:_id", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool
+      .request()
+      .input("_id", req.params._id)
+      .query(`SELECT * FROM lokehoachphanxuong WHERE _id = @_id`);
+    let lokehoach = result.recordset[0];
+    if (lokehoach) {
+      await pool
+        .request()
+        .input("_id", req.params._id)
+        .input("status", req.body.status)
+        .input("ngayhoanthanhtt", req.body.ngayhoanthanhtt)
+        .query(
+          `UPDATE lokehoachphanxuong SET 
+                        status = @status, 
+                        ngayhoanthanhtt = @ngayhoanthanhtt
+                        WHERE _id = @_id;`
+        );
+      res.json({
+        success: true,
+        message: "Update success !",
+      });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // cập nhật kế hoạch nhà máy anchor
 router.patch("/kehoach/:_id", async (req, res) => {
   try {

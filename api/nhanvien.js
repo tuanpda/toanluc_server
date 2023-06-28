6a68285b54ec2ebb9df721e2970d4552ba1157dc
Your Name <you@example.com>
2023-06-28 12:30:39 +0700

    commit

--- api/nhanvien.js
+++ api/nhanvien.js
@@ -96,14 +96,14 @@ router.patch("/luongcoban/:_id", async (req, res) => {
       await pool
         .request()
         .input("_id", req.params._id)
-        .input("luongcb", req.body.luongcb)
+        .input("mucluong", req.body.mucluong)
         .input("mucluong", req.body.mucluong)
         .input("luongngay", req.body.luongngay)
         .input("bacluong", req.body.bacluong)
         .input("luongtrachnhiem", req.body.luongtrachnhiem)
         .query(
           `UPDATE nhanvien SET 
-              luongcb = @luongcb,
+              mucluong = @mucluong,
               mucluong = @mucluong,
               luongngay =@luongngay,
               bacluong = @bacluong,

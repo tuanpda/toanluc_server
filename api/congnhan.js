9e6158fc8f6cdc799d1b6b2f90ae947e82c9e84a
Your Name <you@example.com>
2023-08-09 11:20:58 +0700

    commit

--- api/congnhan.js
+++ api/congnhan.js
@@ -903,34 +903,35 @@ router.delete("/:_id", async (req, res) => {
 // delete chấm công phân xưởng
 router.delete("/ngaychamcongphanxuong", async (req, res) => {
   try {
-    await pool.connect();
-    const result = await pool
-      .request()
-      .input("ngaychamcong", req.query.ngaychamcong)
-      .input("mapx", req.query.mapx)
-      .query(
-        `SELECT * FROM chamcong WHERE ngaychamcong = @ngaychamcong and mapx=@mapx`
-      );
-    let chamcongngay = result.recordset.length ? result.recordset[0] : null;
-    console.log(chamcongngay);
-    if (chamcongngay) {
-      await pool
-        .request()
-        .input("ngaychamcong", req.query.ngaychamcong)
-        .input("mapx", req.query.mapx)
-        .query(
-          `DELETE FROM chamcong WHERE ngaychamcong = @ngaychamcong and mapx=@mapx;`
-        );
-      res.json({
-        data: chamcongngay,
-        success: true,
-        message: "Xóa thành công",
-      });
-    } else {
-      res.status(404).json({
-        message: "Không tìm thấy",
-      });
-    }
+    console.log("hjhfdsjhf");
+    // await pool.connect();
+    // const result = await pool
+    //   .request()
+    //   .input("ngaychamcong", req.query.ngaychamcong)
+    //   .input("mapx", req.query.mapx)
+    //   .query(
+    //     `SELECT * FROM chamcong WHERE ngaychamcong = @ngaychamcong and mapx=@mapx`
+    //   );
+    // let chamcongngay = result.recordset.length ? result.recordset[0] : null;
+    // console.log(chamcongngay);
+    // if (chamcongngay) {
+    //   await pool
+    //     .request()
+    //     .input("ngaychamcong", req.query.ngaychamcong)
+    //     .input("mapx", req.query.mapx)
+    //     .query(
+    //       `DELETE FROM chamcong WHERE ngaychamcong = @ngaychamcong and mapx=@mapx;`
+    //     );
+    //   res.json({
+    //     data: chamcongngay,
+    //     success: true,
+    //     message: "Xóa thành công",
+    //   });
+    // } else {
+    //   res.status(404).json({
+    //     message: "Không tìm thấy",
+    //   });
+    // }
   } catch (error) {
     res.status(500).json(error);
   }

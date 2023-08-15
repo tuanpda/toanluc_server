384d81b912b60b17fd272d68771aad6af59d3e1a
Your Name <you@example.com>
2023-08-15 13:10:37 +0700

    commit

--- api/ketoan.js
+++ api/ketoan.js
@@ -1011,6 +1011,24 @@ router.get("/getkeythangnam_chitraluong", async (req, res) => {
   }
 });
 
+// lấy keythangnam từ bảng luongthang
+router.get("/getkeythangnamwithdata", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool
+      .request()
+      .input("key_thangnam", req.query.key_thangnam)
+      .query(
+        `select manv+'-'+hotennv+'-'+key_thangnam from luongthang where key_thangnam=@key_thangnam`
+      );
+    const ktn = result.recordset;
+
+    res.json(ktn);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // cập nhật status trong công đoạn lương
 router.patch("/updatestatusluongcongdoan/:_id", async (req, res) => {
   try {

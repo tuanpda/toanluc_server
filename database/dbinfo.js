4249f3feef70f0b128c41a45ab8251c1aae2c31b
Your Name <you@example.com>
2024-04-04 16:20:39 +0700

    fcm

--- database/dbinfo.js
+++ database/dbinfo.js
@@ -1,16 +1,16 @@
 const mssql = require("mssql");
 
-// const SQL_DRIVER = process.env.SQL_DRIVER
-// const SQL_SERVER = process.env.SQL_SERVER
-// const SQL_DATABASE = process.env.SQL_DATABASE
-// const SQL_UID = process.env.SQL_UID
-// const SQL_PWD = process.env.SQL_PWD
+const SQL_DRIVER = process.env.SQL_DRIVER
+const SQL_SERVER = process.env.SQL_SERVER
+const SQL_DATABASE = process.env.SQL_DATABASE
+const SQL_UID = process.env.SQL_UID
+const SQL_PWD = process.env.SQL_PWD
 
-const SQL_DRIVER = "SQL server";
-const SQL_SERVER = "192.168.1.97";
-const SQL_DATABASE = "tinhluong_bu";
-const SQL_UID = "sa";
-const SQL_PWD = "Sa)(%^0702!@";
+// const SQL_DRIVER = "SQL server";
+// const SQL_SERVER = "192.168.1.97";
+// const SQL_DATABASE = "tinhluong_bu";
+// const SQL_UID = "sa";
+// const SQL_PWD = "Sa)(%^0702!@";
 
 /* NOTE IMPORTANT FOR SQLSERVER AFTER REINSTALL 8391
 ENABLE TCP/IP
